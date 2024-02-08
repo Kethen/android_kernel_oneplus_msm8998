231d9e2dca4a0c7cd16ad22ae7673b0e8ff01399
Katharine Chui <kwchuiaa@connect.ust.hk>
2024-02-09 05:34:41 +0800

    dumpling standalone build script

---
+++ dumpling_kernel_build/build_podman.sh
@@ -0,0 +1,18 @@
+SCRIPT_DIR=$(dirname "$0")
+cd "$SCRIPT_DIR"
+
+IMAGE_NAME="dumpling_kernel_build"
+
+if ! podman image exists "$IMAGE_NAME"
+then
+	podman image build -f Dockerfile -t "$IMAGE_NAME"
+fi
+
+cd ../
+
+podman run \
+	--rm -it \
+	-v ./:/workdir \
+	-w /workdir/dumpling_kernel_build \
+	--entrypoint /bin/bash \
+	"$IMAGE_NAME"
